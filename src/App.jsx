07e23167f0ec55780dd153a39bf03df7ffa90c6d
RaihanMiraj <Rsnmiraj@gmail.com>
2023-05-20 13:59:22 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState ,createContext } from 'react'
 import RouteHandle from './Config/RouteHandle';
  
 import AuthContextProvider from './Provider/AuthContextProvider';
- import axios from 'axios'; 
+import axios from 'axios'; 
  axios.defaults.baseURL = "https://toy-market-backend.vercel.app/"
  function App() {
  
